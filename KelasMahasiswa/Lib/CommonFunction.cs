68a04f2d8dbf8f6610aed12fb2bb353dadd168de
Bram Pratowo <Bram Pratowo@DESKTOP-5VD8DHH>
2017-03-26 00:45:14 +0700

    1. CommonFunction untuk list function yang sering digunakan. Menambahkan function FormLoading
    2. Menambahkan model organisasi amikom (fakultas, prodi, program)

---
+++ KelasMahasiswa/Lib/CommonFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KelasMahasiswa.Lib
+{
+    public class CommonFunction
+    {
+        public static void FormLoading(Control parentControl, ProgressBar pb, bool onLoading)
+        {
+            if(onLoading)
+            {
+                parentControl.Enabled = false;
+                pb.Visible = true;
+            }
+            else
+            {
+                parentControl.Enabled = true;
+                pb.Visible = false;
+            }
+        }
+    }
+}
---
+++ KelasMahasiswa/Models/Organisasi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KelasMahasiswa.Models
+{
+    public class Fakultas
+    {
+        public Guid IdFakultas { get; set; }
+        public string KodeFakultas { get; set; }
+        public string NamaFakultas { get; set; }
+    }
+
+    public class ProgramStudi
+    {
+        public Guid Id { get; set; }
+        public string IdProdi { get; set; }
+        public string NamaProdi { get; set; }
+        public string Jenjang { get; set; }
+    }
+
+    public class ProgramProdi
+    {
+        public string KodeProgram { get; set; }
+        public string NamaProgram { get; set; }
+        public Guid IdProdi { get; set; }
+        public string NamaProdi { get; set; }
+    }
+}
